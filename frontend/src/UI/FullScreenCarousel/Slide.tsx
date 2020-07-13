a037a0aff15af07af3fe2d370711fbd564c4b845
Gerald Pape <mail@geraldpape.io>
2020-07-13 21:15:42 +0200

    format

--- frontend/src/UI/FullScreenCarousel/Slide.tsx
+++ frontend/src/UI/FullScreenCarousel/Slide.tsx
@@ -11,27 +11,29 @@ export interface ISlide {
 const Slide: FC<ISlide> = ({ backgroundImage, title }) => (
   <div className={styles.slide}>
     <div className={styles.qrArea}>
-        <QRCode value={title} className={styles.qr} />
+      <QRCode value={title} className={styles.qr} />
     </div>
     <div className={styles.titleArea}>
       <h1 className={styles.title}>{title}</h1>
     </div>
     <div className={styles.infoArea}>
-        <h4>InfoBox</h4>
-        <ul>
-            <li>Was</li>
-            <li>Wann</li>
-            <li>Wo</li>
-            <li>Wer</li>
-            <li>Warum</li>
-        </ul>
+      <h4>InfoBox</h4>
+      <ul>
+        <li>Was</li>
+        <li>Wann</li>
+        <li>Wo</li>
+        <li>Wer</li>
+        <li>Warum</li>
+      </ul>
     </div>
-      <div className={styles.imageArea}>
-          <img alt={`Bild ${title}`} className={styles.image} src={backgroundImage} />
-      </div>
-    <div className={styles.footerArea}>
-
+    <div className={styles.imageArea}>
+      <img
+        alt={`Bild ${title}`}
+        className={styles.image}
+        src={backgroundImage}
+      />
     </div>
+    <div className={styles.footerArea}></div>
   </div>
 );
 
