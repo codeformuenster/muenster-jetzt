1a6ca8399ae6ae44ff8770fb2e47552acfa0ab5c
bCyberBasti <66736282+bCyberBasti@users.noreply.github.com>
2020-07-15 19:25:18 +0200

    Small demo data imrovements (#12)
    * add https_proxy role
    
    * improve gitignore
    
    * add svg icons (not yet used)
    
    * use real url in qr code, expanded event data

--- frontend/src/Kiosk/Kiosk.tsx
+++ frontend/src/Kiosk/Kiosk.tsx
@@ -4,9 +4,15 @@ import FullScreenCarousel from "../UI/FullScreenCarousel/FullScreenCarousel";
 
 const mockData: IEvent[] = [
   {
-    title: "Weihnachtsmarkt",
-    subtitle: "Festlich shoppen in der Innenstadt",
-    imageUrl: "https://placekitten.com/1080/1080",
+    title: "Wochenmarkt",
+    subtitle: "Mittwochsmarkt am Domplatz",
+    start: new Date(2020, 6, 15, 7),
+    end: new Date(2020, 6, 15, 14, 30),
+    description:
+      "Mit rund 150 Ständen ist der Wochenmarkt auf dem Domplatz der größte in Münster. Ein üppiges Angebot zieht mittwochs und besonders samstags die Besucherströme in die Innenstadt: Obst und Gemüse, Fisch und Meeresfrüchte, Gewürze, Öl und Antipasti, unzählige Käsesorten, Honig, Brot und Kuchen, Fleisch und Wurst, Wild und Geflügel und - vor allem im Spätsommer - ein wahres Blumenmeer.",
+    imageUrl:
+      "https://www.wochenmarkt-muenster.de/fileadmin/_processed_/8/f/csm_anfahrt-01_01_849037d810.jpg",
+    externalUrl: "https://www.wochenmarkt-muenster.de/",
   },
   {
     title: "Münsterland Giro",
--- frontend/src/UI/FullScreenCarousel/Slide.tsx
+++ frontend/src/UI/FullScreenCarousel/Slide.tsx
@@ -7,23 +7,26 @@ interface ISlide extends IEvent {
   style?: CSSProperties;
 }
 
-const Slide: FC<ISlide> = ({ imageUrl, title, subtitle, style }) => (
+const Slide: FC<ISlide> = ({
+  imageUrl,
+  title,
+  subtitle,
+  description,
+  externalUrl,
+  style,
+}) => (
   <div className={styles.slide} style={style}>
     <div className={styles.qrArea}>
-      <QRCode value={title} className={styles.qr} />
+      {externalUrl !== undefined && (
+        <QRCode value={externalUrl} className={styles.qr} />
+      )}
     </div>
     <div className={styles.titleArea}>
       <h1 className={styles.title}>{title}</h1>
     </div>
     <div className={styles.infoArea}>
       <h4 className={styles.subTitle}>{subtitle}</h4>
-      <ul className={styles.factsList}>
-        <li>Was</li>
-        <li>Wann</li>
-        <li>Wo</li>
-        <li>Wer</li>
-        <li>Warum</li>
-      </ul>
+      <p className={styles.descriptionTxt}>{description}</p>
     </div>
     <div className={styles.imageArea}>
       <img alt={`Bild ${title}`} className={styles.image} src={imageUrl} />
