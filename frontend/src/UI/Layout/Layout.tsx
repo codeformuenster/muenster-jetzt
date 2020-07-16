4ba3db3e4ad307d4b9d56bb7ca8fcbed17d65e8d
bCyberBasti <66736282+bCyberBasti@users.noreply.github.com>
2020-07-16 23:56:20 +0200

    add deployment job (#19)
    * add deployment job
    
    * Add deployment 2 (#21)
    
    * Add VersionString component
    
    * move deployment to own file
    
    * try fixing git diff in frontend ci
    
    * move draft release into own job & try fix git diff again
    
    * only build containers for master for now
    
    * just add something to trigger ci
    
    * update draft release only for master branch pushes
    
    Co-authored-by: kannix <cyberbrief@posteo.de>
    Co-authored-by: Gerald Pape <ubergesundheit@users.noreply.github.com>
    Co-authored-by: Gerald Pape <mail@geraldpape.io>

--- frontend/src/UI/Layout/Layout.tsx
+++ frontend/src/UI/Layout/Layout.tsx
@@ -1,10 +1,13 @@
 import React, { FC } from "react";
+import VersionString from "../VersionString/VersionString";
+
 import styles from "./Layout.module.css";
 
 const Layout: FC = ({ children }) => (
   <>
     <header className={styles.header}>Münster Jetzt</header>
     <section className={styles.mainSection}>{children}</section>
+    <VersionString />
   </>
 );
 
