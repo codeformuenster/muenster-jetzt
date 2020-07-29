65401f17f649c675236038f4499aaa03713170ed
Jakob de Maeyer <jakobdm1@gmail.com>
2020-07-29 19:05:17 +0200

    Add CLI, peewee models, crawling, and events API endpoint to backend

---
+++ backend/mj/api.py
@@ -0,0 +1,38 @@
+import datetime
+from typing import Optional
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from playhouse.shortcuts import model_to_dict
+
+import mj
+from mj.db import Event
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+@app.get('/')
+def root():
+    return {'version': f'mj-{mj.__version__}'}
+
+
+@app.get('/events')
+def events(
+        min_date: Optional[datetime.date] = None,
+        max_date: Optional[datetime.date] = None,
+        page: int = 1, limit: int = 50):
+    events = Event.select().order_by(Event.start_date, Event.start_time)
+    if min_date:
+        events = events.where(Event.start_date >= min_date)
+    if max_date:
+        events = events.where(Event.start_date <= max_date)
+    events = events.paginate(page, limit)
+    return {'events': [model_to_dict(e) for e in events]}
---
+++ backend/mj/spiders/__init__.py
@@ -0,0 +1,44 @@
+import scrapy
+
+from mj.db import db, Event, EventSource, Location, Organizer
+
+
+class SpiderDefaultsPipeline:
+
+    def process_item(self, item, spider):
+        for k, v in spider.defaults.items():
+            item.setdefault(k, v)
+        return item
+
+
+class DatabaseExportPipeline:
+
+    def process_item(self, item, spider):
+        with db.transaction():
+            values = item.copy()
+            values['location'], _ = Location.get_or_create(
+                description=item['location'])
+            if item['organizer']:
+                values['organizer'], _ = Organizer.get_or_create(
+                    name=item['organizer'])
+            values['source'], _ = EventSource.get_or_create(
+                name=item['source'])
+            event, _ = Event.get_or_create(
+                source=values['source'],
+                source_event_id=values['source_event_id'])
+            for k, v in values.items():
+                setattr(event, k, v)
+            event.save()
+        return item
+
+
+class EventSpider(scrapy.Spider):
+
+    custom_settings = {
+        'ITEM_PIPELINES': {
+            'mj.spiders.SpiderDefaultsPipeline': 100,
+            'mj.spiders.DatabaseExportPipeline': 900,
+        },
+    }
+
+    defaults = {}
