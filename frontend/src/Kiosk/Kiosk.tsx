ac78743066ec22c8bfd52d0c4b94cc5977171dd7
kannix <cyberbrief@posteo.de>
2020-08-06 10:26:09 +0200

    update kiosk content

--- frontend/src/Kiosk/Kiosk.tsx
+++ frontend/src/Kiosk/Kiosk.tsx
@@ -4,6 +4,11 @@ import FullScreenCarousel from "../UI/FullScreenCarousel/FullScreenCarousel";
 
 const mockData: IEvent[] = [
   {
+    id: 0,
+    imageUrl: "https://www.bleker-it.de/download/hygienemassnahmen.jpg",
+    externalUrl: "https://www.gemeinsamgehtdas.de/",
+  },
+  {
     id: 1,
     imageUrl: "https://www.bleker-it.de/download/db_lambus_1.png",
   },
@@ -21,7 +26,40 @@ const mockData: IEvent[] = [
   },
   {
     id: 3,
+    imageUrl: "https://www.bleker-it.de/download/hygienemassnahmen.jpg",
+    externalUrl: "https://www.gemeinsamgehtdas.de/",
+  },
+  {
+    id: 4,
     imageUrl: "https://www.bleker-it.de/download/nafe-qr.jpg",
+  },
+  {
+    id: 5,
+    imageUrl: "https://www.bleker-it.de/download/db_icons.png",
+    externalUrl:
+      "https://gobeta.de/projekte/neue-angebote-am-hauptbahnhof-muenster-ausprobieren-im-sommer-2020/",
+  },
+  {
+    id: 6,
+    imageUrl: "https://www.bleker-it.de/download/hygienemassnahmen.jpg",
+    externalUrl: "https://www.gemeinsamgehtdas.de/",
+  },
+  {
+    id: 7,
+    imageUrl: "https://www.bleker-it.de/download/Mindbox_Vitrine.png",
+  },
+  {
+    id: 8,
+    title: "MÜNSTERHACK 2020",
+    subtitle: "Deine Hacking-Idee für ein (noch) lebenswerteres Münster!",
+    description:
+      "Der MÜNSTERHACK 2020 steht vor der Tür: Reiche noch bis zum 31. August Deine Ideen für Münster ein, indem du den QR-Code scannst ➡️",
+    imageUrl: "https://www.muensterhack.de/img/logo_muensterhack.png",
+    externalUrl:
+      "https://gobeta.de/news/deine-hacking-idee-fuer-ein-noch-lebenswerteres-muenster/",
+    style: {
+      backgroundColor: "#FCDD09",
+    },
   },
 ];
 
