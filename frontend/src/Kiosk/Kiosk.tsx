5c1a25d06c18d40982dd926fb79544375d3fc8d0
Gerald Pape <ubergesundheit@users.noreply.github.com>
2020-08-25 21:49:32 +0200

    Extract query params & use it for selecting different kiosk mockdata sets (#84)
    * add hook for extracting query params & use it for selecting different mock data
    
    * set params without value to value `true`
    
    * Fix types in useQuery hook
    
    * run prettier
    
    * Frontend Tracking (#89)
    
    * fix docker compose and deploy script (#88)
    
    * fix docker compose and deploy script
    
    * change deploy order
    
    * add /track nginx http 204 route
    
    * Bump @testing-library/react from 10.4.7 to 10.4.8 in /frontend (#90)
    
    Bumps [@testing-library/react](https://github.com/testing-library/react-testing-library) from 10.4.7 to 10.4.8.
    - [Release notes](https://github.com/testing-library/react-testing-library/releases)
    - [Changelog](https://github.com/testing-library/react-testing-library/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/testing-library/react-testing-library/compare/v10.4.7...v10.4.8)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * update kiosk content (#95)
    
    * add ky to send slide changes to /track endpoint
    
    * disable caching on /track endpoint
    
    * move kiosk slide tracking to own hook & track clicks
    
    * make prettier happy :)
    
    * types & linter issues
    
    Co-authored-by: bCyberBasti <66736282+bCyberBasti@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * change tracking to be enabled when device parameter is present
    
    Co-authored-by: bCyberBasti <66736282+bCyberBasti@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- frontend/src/Kiosk/Kiosk.tsx
+++ frontend/src/Kiosk/Kiosk.tsx
@@ -1,72 +1,29 @@
-import React, { FC } from "react";
+import React, { FC, useMemo } from "react";
 import KioskLayout from "../UI/KioskLayout/KioskLayout";
 import FullScreenCarousel from "../UI/FullScreenCarousel/FullScreenCarousel";
+import useQuery from "../hooks/useQuery";
 
-const mockData: IEvent[] = [
-  {
-    id: 0,
-    imageUrl: "https://www.bleker-it.de/download/hygienemassnahmen.jpg",
-    externalUrl: "https://www.gemeinsamgehtdas.de/",
-  },
-  {
-    id: 1,
-    imageUrl: "https://www.bleker-it.de/download/db_lambus_1.png",
-  },
-  {
-    id: 2,
-    title: "MÜNSTERHACK 2020",
-    subtitle: "Hackathon Für Münster",
-    description:
-      "Beim MÜNSTERHACK kommt die Tech-Szene in Münster zusammen und entwickelt gemeinsam Ideen und Prototypen, um die eigene Stadt noch lebenswerter zu machen.\n\n25./26. SEPTEMBER 2020",
-    imageUrl: "https://www.muensterhack.de/img/logo_muensterhack.png",
-    externalUrl: "https://gobeta.de/veranstaltungen/muensterhack-2020/",
-    style: {
-      backgroundColor: "#FCDD09",
-    },
-  },
-  {
-    id: 3,
-    imageUrl: "https://www.bleker-it.de/download/hygienemassnahmen.jpg",
-    externalUrl: "https://www.gemeinsamgehtdas.de/",
-  },
-  {
-    id: 4,
-    imageUrl: "https://www.bleker-it.de/download/nafe-qr.jpg",
-  },
-  {
-    id: 5,
-    imageUrl: "https://www.bleker-it.de/download/db_icons.png",
-    externalUrl:
-      "https://gobeta.de/projekte/neue-angebote-am-hauptbahnhof-muenster-ausprobieren-im-sommer-2020/",
-  },
-  {
-    id: 6,
-    imageUrl: "https://www.bleker-it.de/download/hygienemassnahmen.jpg",
-    externalUrl: "https://www.gemeinsamgehtdas.de/",
-  },
-  {
-    id: 7,
-    imageUrl: "https://www.bleker-it.de/download/Mindbox_Vitrine.png",
-  },
-  {
-    id: 8,
-    title: "MÜNSTERHACK 2020",
-    subtitle: "Deine Hacking-Idee für ein (noch) lebenswerteres Münster!",
-    description:
-      "Der MÜNSTERHACK 2020 steht vor der Tür: Reiche noch bis zum 31. August Deine Ideen für Münster ein, indem du den QR-Code scannst ➡️",
-    imageUrl: "https://www.muensterhack.de/img/logo_muensterhack.png",
-    externalUrl:
-      "https://gobeta.de/news/deine-hacking-idee-fuer-ein-noch-lebenswerteres-muenster/",
-    style: {
-      backgroundColor: "#FCDD09",
-    },
-  },
-];
+import { mockData, mockData2 } from "./mockdata";
+
+const useMockData = () => {
+  const query = useQuery();
+
+  return useMemo(() => {
+    switch (query?.device) {
+      case "ms-display1":
+        return mockData2;
+      default:
+        return mockData;
+    }
+  }, [query]);
+};
 
 const Kiosk: FC = () => {
+  const mockedSlideData = useMockData();
+
   return (
     <KioskLayout>
-      <FullScreenCarousel slides={mockData} />
+      <FullScreenCarousel slides={mockedSlideData} />
     </KioskLayout>
   );
 };
