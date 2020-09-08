42e50e5bc0559be8762315a5cb4a3f66f6f9bae8
Thorben Jensen <jensen.thorben@gmail.com>
2020-09-08 22:51:37 +0200

    add comments to outline TODOs

--- backend/mj/scrape.py
+++ backend/mj/scrape.py
@@ -5,15 +5,21 @@ import os
 import dotenv
 import requests
 
+# credentials for API connection
 dotenv.load_dotenv()
 URL = "https://www.datenportal-muensterland.de/api/v1/events"
 USER = os.getenv("DATENPORTAL_USER")
 PASSWORD = os.getenv("DATENPORTAL_PASSWORD")
 
+# read events from API
 response = requests.get(URL, auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
-
 if response.status_code != 200:
     raise requests.HTTPError
-
 response_dict: dict = response.json()
 events: list = response_dict["data"]
+
+# parse to Event objects
+# TODO
+
+# save to database
+# TODO
