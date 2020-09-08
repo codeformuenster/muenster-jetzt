ae07d4c4781d1e87d9f76f6377c37cedc9434c35
Thorben Jensen <jensen.thorben@gmail.com>
2020-09-08 22:39:19 +0200

    loading list of events from API

--- backend/mj/scrape.py
+++ backend/mj/scrape.py
@@ -1,12 +1,19 @@
+""" Scraping event API of https://www.muensterland.digital/ """
+
+import os
+
+import dotenv
 import requests
 
+dotenv.load_dotenv()
 URL = "https://www.datenportal-muensterland.de/api/v1/events"
+USER = os.getenv("DATENPORTAL_USER")
+PASSWORD = os.getenv("DATENPORTAL_PASSWORD")
 
-reponse = requests.get(URL, auth=requests.auth.HTTPBasicAuth('user', 'password'))
+response = requests.get(URL, auth=requests.auth.HTTPBasicAuth(USER, PASSWORD))
 
-if reponse.status_code != 200:
+if response.status_code != 200:
     raise requests.HTTPError
 
-response_dict = reponse.json()
-
-response_dict.keys()
+response_dict: dict = response.json()
+events: list = response_dict["data"]
