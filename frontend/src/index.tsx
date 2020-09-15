6a3b7aff1e0c0f579da49c1a5ef405ab77ce946c
Gerald Pape <mail@geraldpape.io>
2020-09-15 18:53:08 +0200

    redirect plain /app to todays /app/YYYY-MM-DD

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { RestfulProvider } from "restful-react";
 
 import App from "./App/App";
@@ -11,12 +16,17 @@ import TrackingWrapper from "./TrackingWrapper";
 const backendBasePath =
   process?.env?.REACT_APP_BACKEND_BASE_PATH || "http://localhost:8000";
 
+const isoToday = new Date().toISOString().slice(0, 10);
+
 ReactDOM.render(
   <React.StrictMode>
     <RestfulProvider base={backendBasePath}>
       <Router>
         <TrackingWrapper />
         <Switch>
+          <Route exact path="/app">
+            <Redirect to={`/app/${isoToday}/`} />
+          </Route>
           <Route path="/app/:date">
             <App />
           </Route>
