de8d50f19b4f6b8835c3ab62ce04c80935f0d5af
Thomas Werner <thomas.w.werner@gmail.com>
2020-09-15 22:13:37 +0200

    Add Spider for Muensterland e.V. (="Datenportal Münsterland")

---
+++ backend/mj/spiders/muensterland_ev.py
@@ -0,0 +1,63 @@
+import os
+
+import scrapy
+from scrapy.exceptions import CloseSpider
+from w3lib.http import basic_auth_header
+from datetime import date
+
+from mj.spiders import EventSpider
+
+
+class MuensterlandEvSpider(EventSpider):
+
+    name = 'muensterland_ev'
+
+    defaults = {
+        'source': 'Münsterland e.V.',
+        'source_license': '?',
+    }
+
+    def start_requests(self):
+
+        if not (os.getenv('DATENPORTAL_USER') and  os.getenv("DATENPORTAL_PASSWORD")):
+            self.logger.error(
+                "Please set the DATENPORTAL_USER and DATENPORTAL_PASSWORD environment variables")
+            raise CloseSpider('missing_auth')
+
+        URL = "https://www.datenportal-muensterland.de/api/v1/events"
+        USER = os.getenv("DATENPORTAL_USER")
+        PASSWORD = os.getenv("DATENPORTAL_PASSWORD")
+
+        # read events from API
+        today = date.today()
+        event_api_url = URL + '?filter[starts_after]=' + today.strftime('%Y-%m-%d') \
+            + '&sort=events.start_datetime' \
+            + '&page[size]=20'
+
+        auth = basic_auth_header(USER, PASSWORD)
+
+        yield scrapy.Request(event_api_url, headers={'Authorization': auth})
+
+    def parse(self, response):
+        for event in response.json()['data']:
+            start_datetime = event['start_datetime'].split('T')
+            end_datetime = event['end_datetime'].split('T')
+            address = event['poi']['name']
+            if event['poi']['address']['postal_code'] and event['poi']['address']['city']:
+                address += "\n" + str(event['poi']['address']['postal_code']) \
+                    + ' ' + str(event['poi']['address']['city'])
+
+            yield {
+                'source_event_id': 'mst-ev-' + str(event['id']),
+                'name': event['name'],
+                'description': event['description_text'],
+                'url': event['external_link'],
+                'start_date': start_datetime[0],
+                'start_time': start_datetime[1],
+                'end_date': end_datetime[0],
+                'end_time': end_datetime[1],
+                'location': address,
+                'performer': '',
+                'mode': event['types'][0]['name'] if ('types' in event) and len(event['types'])>0 else '',
+                'organizer': event['event_organizers'][0]['name'] if ('event_organizers' in event) and len(event['event_organizers'])>0 else ''
+            }
