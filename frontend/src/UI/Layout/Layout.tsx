a03ab62a11d34a3dc6fc49fa724f791587a81551
Uria Rogel <uria_rogel@hotmail.com>
2020-09-18 15:17:16 +0200

    title based on date WIP

--- frontend/src/UI/Layout/Layout.tsx
+++ frontend/src/UI/Layout/Layout.tsx
@@ -1,25 +1,35 @@
 import React, { FC } from "react";
-
+import { useLocation, useParams } from 'react-router-dom'
 import "../../styles/base.scss";
 import styles from "./Layout.module.scss";
 
-import logo from "../../images/ms-jetzt-logo.svg";
+import logo from "../../images/ms-jetzt-logo-icon.svg";
 
 interface ILayout {
   hideHeader?: boolean;
 }
 
-const Layout: FC<ILayout> = ({ children, hideHeader }) => (
-  <>
-    {!hideHeader && (
-      <header className={styles.header}>
-        <img src={logo} alt="Münster Jetzt" className={styles.logo} />
-      </header>
-    )}
-    <section className={styles.mainSection}>
-      <div className={styles.container}>{children}</div>
-    </section>
-  </>
-);
+const Layout: FC<ILayout> = ({ children, hideHeader }) => {
+  const urlParams = useParams();
+  // TODO: Gerald - types help
+  const date = urlParams.date.split('-');
+  const dayMonth = `${date[2]}.${date[1]}`;
+  
+  return (
+    <>
+      {!hideHeader && (
+        <header className={styles.header}>
+          <img src={logo} alt="Münster Jetzt" className={styles.logo} />
+          <div>
+            <h4 className={styles.title}>Veranstaltungen am {dayMonth}</h4>
+          </div>
+        </header>
+      )}
+      <section className={styles.mainSection}>
+        <div className={styles.container}>{children}</div>
+      </section>
+    </>
+  )
+};
 
 export default Layout;
