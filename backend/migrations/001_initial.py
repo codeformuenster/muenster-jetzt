bd99b708ba74aedd9621fcdf3838b69da1f87cd0
Thorben Jensen <jensen.thorben@gmail.com>
2020-10-02 22:33:00 +0200

    apply black

--- backend/migrations/001_initial.py
+++ backend/migrations/001_initial.py
@@ -4,7 +4,6 @@ import peewee as pw
 
 
 def migrate(migrator, database, fake=False, **kwargs):  # skipcq: PYL-W0613
-
     @migrator.create_model
     class EventSource(pw.Model):  # skipcq: PYL-W0612
         id = pw.AutoField()
@@ -33,10 +32,11 @@ def migrate(migrator, database, fake=False, **kwargs):  # skipcq: PYL-W0613
     class Event(pw.Model):  # skipcq: PYL-W0612
         id = pw.AutoField()
         source = pw.ForeignKeyField(
-            backref='event_set',
-            column_name='source_id',
-            field='id',
-            model=migrator.orm['eventsource'])
+            backref="event_set",
+            column_name="source_id",
+            field="id",
+            model=migrator.orm["eventsource"],
+        )
         source_event_id = pw.CharField(max_length=255)
         source_url = pw.CharField(max_length=4095, null=True)
         source_license = pw.CharField(max_length=255, null=True)
@@ -48,26 +48,28 @@ def migrate(migrator, database, fake=False, **kwargs):  # skipcq: PYL-W0613
         end_date = pw.DateField(null=True)
         end_time = pw.TimeField(null=True)
         location = pw.ForeignKeyField(
-            backref='event_set',
-            column_name='location_id',
-            field='id',
-            model=migrator.orm['location'])
+            backref="event_set",
+            column_name="location_id",
+            field="id",
+            model=migrator.orm["location"],
+        )
         performer = pw.CharField(max_length=255, null=True)
         mode = pw.CharField(max_length=255, null=True)
         organizer = pw.ForeignKeyField(
-            backref='event_set',
-            column_name='organizer_id',
-            field='id',
-            model=migrator.orm['organizer'],
-            null=True)
+            backref="event_set",
+            column_name="organizer_id",
+            field="id",
+            model=migrator.orm["organizer"],
+            null=True,
+        )
 
         class Meta:
             table_name = "event"
-            indexes = [(('source', 'source_event_id'), True)]
+            indexes = [(("source", "source_event_id"), True)]
 
 
 def rollback(migrator, database, fake=False, **kwargs):  # skipcq: PYL-W0613
-    migrator.remove_model('event')
-    migrator.remove_model('organizer')
-    migrator.remove_model('location')
-    migrator.remove_model('eventsource')
+    migrator.remove_model("event")
+    migrator.remove_model("organizer")
+    migrator.remove_model("location")
+    migrator.remove_model("eventsource")
