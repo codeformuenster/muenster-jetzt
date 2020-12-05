2e208c085a32d531f58d79a82f55612dea600317
Jakob de Maeyer <jakobdm1@gmail.com>
2020-12-05 19:32:05 +0100

    Add hardcoded 400 and 404 response schemas to OpenAPI spec

--- backend/events/schema.py
+++ backend/events/schema.py
@@ -20,3 +20,44 @@ class CamelizingAutoSchema(AutoSchema):
         ]
 
         return new_result
+
+    def _get_parameters(self, path, method):
+        parameters = []
+        parameters += self.get_path_parameters(path, method)
+        parameters += self.get_pagination_parameters(path, method)
+        parameters += self.get_filter_parameters(path, method)
+        return [p["name"] for p in parameters]
+
+    def get_responses(self, path, method):
+        responses = super().get_responses(path, method)
+        responses["200"]["description"] = "Success"
+        # Rest Framework does not include error schemas (as their exception
+        # handler is a simple view, not a serializer from which a schema could
+        # be inferred)
+        responses["400"] = {
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "type": "object",
+                        "properties": {
+                            p: {"type": "array", "items": {"type": "string"}}
+                            for p in self._get_parameters(path, method)
+                        },
+                    }
+                }
+            },
+            "description": "Bad Request",
+        }
+        responses["404"] = {
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "type": "object",
+                        "properties": {"detail": {"type": "string"}},
+                        "required": ["detail"],
+                    }
+                }
+            },
+            "description": "Not Found",
+        }
+        return responses
