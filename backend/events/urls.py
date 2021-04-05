00040f89f7df20b01e746a06376974fd0017b3d1
Gerald Pape <mail@geraldpape.io>
2021-04-05 20:00:06 +0200

    add /docs swagger-ui

--- backend/events/urls.py
+++ backend/events/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 from . import views
 
@@ -8,6 +9,14 @@ router.register(r"events", views.EventsViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("openapi.json", views.schema_json),
+    path("openapi.json", views.schema_json, name="openapi.json"),
     path("openapi.yml", views.schema_yml),
+    path(
+        "docs/",
+        TemplateView.as_view(
+            template_name="swagger-ui.html",
+            extra_context={"schema_url": "openapi.json"},
+        ),
+        name="swagger-ui",
+    ),
 ]
