d9df221f9a6455083eb3ec9f8bd26f98b7511be4
top-on <top-on@posteo.de>
2022-09-23 19:38:53 +0200

    fix flake8 error

--- backend/scraping/spiders/__init__.py
+++ backend/scraping/spiders/__init__.py
@@ -1,8 +1,11 @@
+"""General spider functionality."""
+
 import logging
 
 import bleach
 import scrapy
 from django.db import transaction
+from geopy.geocoders import Nominatim
 from scrapy.exceptions import DropItem
 
 from events.models import Event, EventSource, Location, Organizer
@@ -63,9 +66,6 @@ class SanitizeHTMLPipeline:
                 )
                 raise DropItem(f"Unexpected HTML in {field}")
         return item
-
-
-from geopy.geocoders import Nominatim
 
 
 class DatabaseExportPipeline:
